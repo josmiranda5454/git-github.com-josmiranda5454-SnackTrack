aa3312f65c72c218edf28ea9a5409ee14930969f
Jose Miranda <jose.miranda5454@gmail.com>
2022-04-06 21:45:55 -0400

    Initial Commit

---
+++ SnackTrack/SnackTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnackTrackApp.swift
+//  SnackTrack
+//
+//  Created by Jose Miranda on 4/6/22.
+//
+
+import SwiftUI
+
+@main
+struct SnackTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
